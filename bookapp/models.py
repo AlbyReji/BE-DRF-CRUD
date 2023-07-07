1af8ffcd82a6b907893daaba2e55525909691305
AlbyReji <albymathew30@gmail.com>
2023-07-07 13:52:01 +0530

    commit

--- bookapp/models.py
+++ bookapp/models.py
@@ -3,12 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-class Book(models.Model):
-    title = models.CharField(max_length = 100)
-    description =models.CharField(max_length = 250)
-    author = models.CharField(max_length = 100)
-    Created_at = models.DateTimeField(auto_now_add = True)
-
 class Books(models.Model):
     title = models.CharField(max_length = 100)
     description =models.CharField(max_length = 250)
